348a1abee2c9ef5d099f89823aa613b067b18b30
Swiip <matthieu.lux@gmail.com>
2016-08-13 13:39:00 +0200

    handle token renew in the stream

--- freebox.js
+++ freebox.js
@@ -1,6 +1,5 @@
 /* eslint camelcase: 0, babel/new-cap: 0 */
 
-const Rx = require('rxjs/Rx');
 const superagent = require('superagent');
 const {HmacSHA1} = require('crypto-js');
 const {
@@ -13,59 +12,46 @@ const {
 	serverPassword
 } = require('./conf');
 
-let sessionToken = null;
-let sessionDate = null;
-
 exports.login = function login() {
-	if (sessionToken === null && (sessionDate === null || sessionDate.getTime() + (60 * 60 * 100) < new Date().getTime())) {
-		console.log('Login to', `${freeboxUrl}/api/v3/login/`);
-		sessionDate = new Date();
-		return superagent
-			.get(`${freeboxUrl}/api/v3/login/`)
-			.observe()
-			.map(res => res.body.result.challenge)
-			.do(challenge => console.log('Login with', challenge, freeboxToken))
-			.flatMap(challenge => {
-				return superagent
-					.post(`${freeboxUrl}/api/v3/login/session/`)
-					.send({
-						app_id: freeboxAppId,
-						password: HmacSHA1(challenge, freeboxToken).toString()
-					});
-			})
-			.map(res => res.body.result.session_token)
-			.do(token => {
-				sessionToken = token;
-				console.log('Token', token);
-			});
-	}
-
-	console.log('Return session token');
-	return Rx.Observable.of(sessionToken);
+	console.log('Login to', `${freeboxUrl}/api/v3/login/`);
+	return superagent
+		.get(`${freeboxUrl}/api/v3/login/`)
+		.observe()
+		.map(res => res.body.result.challenge)
+		.do(challenge => console.log('Login with', challenge, freeboxToken))
+		.flatMap(challenge => {
+			return superagent
+				.post(`${freeboxUrl}/api/v3/login/session/`)
+				.send({
+					app_id: freeboxAppId,
+					password: HmacSHA1(challenge, freeboxToken).toString()
+				})
+				.observe();
+		})
+		.map(res => res.body.result.session_token)
+		.do(token => console.log('Token', token));
 };
 
-exports.upload = function upload(download_url) {
-	return exports.login()
-		.flatMap(sessionToken => {
-			const relative = download_url.replace(serverUrl + serverContext, '');
-			const directory = relative.substring(0, relative.lastIndexOf('/'));
-			const download_dir = new Buffer(`/Disque dur/Téléchargements/${directory}`).toString('base64');
-			console.log('Upload', download_url, 'to', directory);
-			return superagent
-				.post(`${freeboxUrl}/api/v3/downloads/add`)
-				.set('X-Fbx-App-Auth', sessionToken)
-				.type('form')
-				.send({
-					download_url,
-					download_dir,
-					username: serverUsername,
-					password: serverPassword
-				});
+exports.upload = function upload(download_url, token) {
+	const relative = download_url.replace(serverUrl + serverContext, '');
+	const directory = relative.substring(0, relative.lastIndexOf('/'));
+	const download_dir = new Buffer(`/Disque dur/Téléchargements/${directory}`).toString('base64');
+	console.log('Upload', download_url, 'to', directory);
+	return superagent
+		.post(`${freeboxUrl}/api/v3/downloads/add`)
+		.set('X-Fbx-App-Auth', token)
+		.type('form')
+		.send({
+			download_url,
+			download_dir,
+			username: serverUsername,
+			password: serverPassword
 		})
+		.observe()
 		.do(res => console.log('Upload response', res.body));
 };
 
-exports.token = function token() {
+exports.newAppToken = function newAppToken() {
 	return superagent
 		.get(`${freeboxUrl}/api/v3/login/authorize/`)
 		.send({
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const Rx = require('rxjs/Rx');
 const {watch} = require('./utils');
-const {upload} = require('./freebox');
+const {login, upload} = require('./freebox');
 const {watchDirectory, serverUrl, serverContext} = require('./conf');
 
 const controller = new Rx.Subject();
+
+const token = Rx.Observable.timer(0, 60 * 60 * 1000)
+	.flatMap(() => login());
 
 watch()
 	.zip(controller, x => x)
 	.do(path => console.log('Watched', path))
 	.map(path => path.replace(watchDirectory, serverUrl + serverContext))
 	.do(url => console.log('Rewrite', url))
-	.flatMap(url => upload(url))
+	.combineLatest(token, (url, token) => ({url, token}))
+	.distinctUntilChanged((a, b) => a.url === b.url)
+	.flatMap(({url, token}) => upload(url, token))
 	.do(() => controller.next())
 	.subscribe(
 		x => x,
